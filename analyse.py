bc1eaf76480361be4bc1544c4d5ae632ee286e26
vboxuser <adityasharma1272008@gmail.com>
2026-08-04 15:55:22 +0000

    added average and highest marks

--- analyse.py
+++ analyse.py
@@ -1,8 +1,23 @@
 import csv 
 
 with open("data.csv",'r') as file:
-	 reader = csv.reader(file) 
+	reader = csv.reader(file) 
 
-	 for row in reader:
+	Total=0
+	count=0
+	highest=0
+	highest_name=""
+	for row in reader:
+             Total=Total + int(row[1])
+             count=count + 1
+             current_marks=int(row[1])
+             current_name=row[0]
+             if current_marks > highest:
+                     highest=current_marks
+                     highest_name=current_name
+
              print(row) 
 
+average=Total/count
+print({"average marks": average})
+print({"highest marks": highest, "name of student with highest marks": highest_name})
